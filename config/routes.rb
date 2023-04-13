116954f63bc84abd026987c37983d7d5bfbacb16
Felipe Arakaki <felipearakaki@outlook.com>
2023-04-13 16:00:42 -0300

    method delete doesnt work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  # List all movies
+  get 'movies', to: 'movies#index'
+  get 'movies/new', to: 'movies#new'
+  get 'movies/:id', to: 'movies#show', as: 'movie'
+  post 'movies', to: 'movies#create'
+  get 'movies/:id/edit', to: 'movies#edit'
+  patch 'movies/:id', to: 'movies#update'
+  delete 'movies/:id', to: 'movies#destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
